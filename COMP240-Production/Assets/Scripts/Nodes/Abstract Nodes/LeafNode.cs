d77e8b4b8383cd8fd6c90fbe8ed2a42a3ff2e1ab
Maddie <mk186442@falmouth.ac.uk>
2017-01-31 11:20:23 +0000

    Added Falcon assets

---
+++ COMP240-Production/Assets/Scripts/Nodes/Abstract Nodes/LeafNode.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeafNode : Node {
+    Companion companion;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Series of functions to check conditions required for different nodes and branches
+    //! checks whether the health is above a certain value
+    bool checkHealth()
+    {
+        if (companion.getHealth() < 1)
+            return companion.isAlive = false;
+        else
+            return true;
+    }
+    //! checks for player input
+    bool checkForInput()
+    {
+        // TODO
+        return false;
+    }
+
+    //! returns bool that indicates whether there's an enemy near the falcon
+    bool checkForEnemy()
+    {
+        // TODO
+        return false;
+    }
+}
---
+++ COMP240-Production/Assets/Scripts/Nodes/BehaviourManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourManager : SelectorNode {
+    GameObject companion;
+    Node.NodeStates currentBranchState;
+
+	// Use this for initialization
+	void Start () {
+        //childNodes = (new AttackBranch, IdleBranch, FishingBranch, DeadBranch); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    Node.NodeStates updateBranches()
+    {
+        checkNodeState();
+        return Node.NodeStates.Running;
+    }
+}
