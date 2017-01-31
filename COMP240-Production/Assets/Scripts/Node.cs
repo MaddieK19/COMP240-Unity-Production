7c360446435e74c846a1ff874dc16203f0864b9b
Maddie <mk186442@falmouth.ac.uk>
2017-01-31 09:23:17 +0000

    Added Node abstract class

---
+++ COMP240-Production/Assets/Scripts/Node.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class Node : MonoBehaviour {
+    public enum NodeStates { Success, Failure, Running };
+    public NodeStates nodeState = NodeStates.Running;
+    abstract public Node.NodeStates updateChildNodes();
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+}
