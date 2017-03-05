cb89ef58f03a017b6548080e7a56b1373bc4872c
Maddie <mk186442@falmouth.ac.uk>
2017-03-05 13:14:44 +0000

    Commented some of the LeafNodes

--- Assets/Scripts/LeafNodes/CheckForInput.cs
+++ Assets/Scripts/LeafNodes/CheckForInput.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using BehaviourMachine;
 using System;
+// Action node for checking for player input
+// Checks if player has issued command to companion
 
-public class CheckForInput : ActionNode
-{
-    // Action node for checking for player input
-    // Checks if player has issued command to companion
-
+public class CheckForInput : ActionNode {
+   // String for what key needs to be pressed
     public StringVar key;
+    // returns Success Status if the player has pressed the specified key
     public override Status Update()
     {
         if (Input.GetKeyDown(key))
