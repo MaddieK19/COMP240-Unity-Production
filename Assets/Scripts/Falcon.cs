7eb1a917a2f46d3f886d629b6616b35559487741
Maddie <mk186442@falmouth.ac.uk>
2017-03-13 09:44:02 +0000

    Removed duplicate code

--- Assets/Scripts/Falcon.cs
+++ Assets/Scripts/Falcon.cs
@@ -5,8 +5,6 @@ using UnityEngine;
  *  Falcon class for the falcon version of the companion AI
  */
 public class Falcon : Companion {
-    // SerialController to send data to Arduino
-    public SerialController serialController;
 
     // Use this for initialization
     void Start () {
@@ -17,14 +15,6 @@ public class Falcon : Companion {
 	public override void Update () {
         // healthCap keeps the companions health positive
         healthCap();
-    }
-
-    // Checks for collision
-    // If colliding with the Player data is sent to the Arduino
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.tag == "Player")
-            serialController.SendSerialMessage("p");
     }
 
 }
