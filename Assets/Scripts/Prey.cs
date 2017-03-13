41e850ecb861c6bcf7c42667323f407016c21038
Maddie <mk186442@falmouth.ac.uk>
2017-03-13 11:06:20 +0000

    Moved TriggerSerial.cs

--- Assets/Scripts/Prey.cs
+++ Assets/Scripts/Prey.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 /* Base class for the companions prey
  * TODO: Make Prey an abstract class and have different types of prey
  */
-public class Prey : MonoBehaviour {
-    public GameObject prey;
+public class Prey : MonoBehaviour
+{
+    public GameObject falcon;
+    int health = 100;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Predator")
+        {
+            transform.parent = falcon.transform;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Predator")
+        {
+            transform.parent = null;
+        }
     }
 }
