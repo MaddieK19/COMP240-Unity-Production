1128ec3fc050129da2064ec63f4ea1c3af194dd9
Maddie <mk186442@falmouth.ac.uk>
2017-04-03 10:07:36 +0100

    Added node that checks for Vive controller input

---
+++ Assets/Scripts/LeafNodes/CheckForViveInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+using System;
+
+public class CheckForViveInput : ActionNode
+{
+    public SteamVR_TrackedController trackedController;
+
+
+    public override Status Update()
+    {
+        if (trackedController.triggerPressed)
+            return Status.Success;
+        else
+            return Status.Failure;
+    }
+}
