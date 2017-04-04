dbf75a8c57ff80479c369c86dab86339ddef5c6a
teasterbrook17 <tseasterbrook17@gmail.com>
2017-04-04 11:18:10 +0100

    objects spawning in

--- Assets/Scripts/Scripts TE/Hexgrid.cs
+++ Assets/Scripts/Scripts TE/Hexgrid.cs
@@ -4,6 +4,9 @@ using UnityEngine.UI;
 public class Hexgrid : MonoBehaviour
 {
 
+    public GameObject mountainPre;
+    public GameObject tree;
+    public GameObject mountainPrefab;
     public int width = 1;
     public int height = 1;
 
@@ -32,10 +35,29 @@ public class Hexgrid : MonoBehaviour
             }
         }
     }
-    
+
     void Start()
     {
         hexMesh.Triangulate(cells);
+        for (int i = 1; i <= 7; i++)
+        {
+            GameObject cube = Instantiate(mountainPre, transform.position, Quaternion.identity) as GameObject;
+            cube.transform.position = new Vector3(Random.Range(-80, -17), -0.1f, Random.Range(-30, 70));
+            cube.transform.localScale = new Vector3(Random.Range(1, 2), Random.Range(1, 2), Random.Range(1, 2));
+        }
+        hexMesh.Triangulate(cells);
+        for (int i = 1; i <= 3; i++)
+        {
+            GameObject cube2 = Instantiate(mountainPrefab, transform.position, Quaternion.identity) as GameObject;
+            cube2.transform.position = new Vector3(Random.Range(7, 46), -0.1f, Random.Range(-15, 40));
+            cube2.transform.localScale = new Vector3(Random.Range(1, 2), Random.Range(1, 2), Random.Range(1, 2));
+        }
+        for (int i = 1; i <= 16; i++)
+        {
+            GameObject cube3 = Instantiate(tree, tree.transform.position, Quaternion.identity) as GameObject;
+            tree.transform.position = new Vector3(Random.Range(-30, 40), -0.1f, Random.Range(-17, 30));
+            tree.transform.localScale = new Vector3(Random.Range(1, 2), Random.Range(1, 2), Random.Range(1, 2));
+        }
     }
 
     public void ColorCell(Vector3 position, Color color)
@@ -50,9 +72,6 @@ public class Hexgrid : MonoBehaviour
 
     void CreateCell(int x, int z, int i)
     {
-        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.position = new Vector3(Random.Range(-10,30), 0, Random.Range(-10, 30));
-        cube.transform.localScale = new Vector3(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
 
         Vector3 position;
         position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
