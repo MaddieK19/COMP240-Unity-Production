54e945d41237cf919955b58a1630204ff4ac40e5
Maddie <mk186442@falmouth.ac.uk>
2017-04-12 17:22:14 +0100

    Fixed issue where falcon would get stuck in the attack animation

--- Assets/Scripts/LeafNodes/ChangeAnimation.cs
+++ Assets/Scripts/LeafNodes/ChangeAnimation.cs
@@ -13,26 +13,29 @@ public class ChangeAnimation : ActionNode
     public String animationName;
     //! Bool for whether the animation should loop or play once
     public bool loopAnimation;
+    //! Bool for whether the animation should play after the previous one ended
+    public bool waitForAnimationEnd;
     //! GameObject for the companion
     public GameObject companion;
 
     public override Status Update()
     {
         if (animationName == null)
-        {
             return Status.Failure;
-        }
+
         if (!loopAnimation)
-        {
             companion.GetComponent<Animation>().wrapMode = WrapMode.ClampForever;
-        }
+
         else if (loopAnimation)
-        {
             companion.GetComponent<Animation>().wrapMode = WrapMode.Loop;
-        }
-        if (companion.GetComponent<Animation>().clip.name != animationName)
+
+        if (!waitForAnimationEnd)
             companion.GetComponent<Animation>().CrossFade(animationName);
-                
+
+        else if (waitForAnimationEnd)
+            companion.GetComponent<Animation>().CrossFadeQueued(animationName);
+
         return Status.Success;
     }
+    
 }
