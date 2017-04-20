e6a77409e0980f39dce0400d164bd90ed3a0079c
Maddie <mk186442@falmouth.ac.uk>
2017-04-20 14:36:34 +0100

    Updated LeafNodes from AI branch

--- Assets/Scripts/LeafNodes/ChangeAnimation.cs
+++ Assets/Scripts/LeafNodes/ChangeAnimation.cs
@@ -3,28 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using BehaviourMachine;
 using System;
+/*!
+ *  Leafnode that changes the animation of a companion
+ */
 
 public class ChangeAnimation : ActionNode
 {
+    //! String for the name of the new animation
     public String animationName;
+    //! Bool for whether the animation should loop or play once
     public bool loopAnimation;
+    //! Bool for whether the animation should play after the previous one ended
+    public bool waitForAnimationEnd;
+    //! GameObject for the companion
     public GameObject companion;
+
     public override Status Update()
     {
         if (animationName == null)
-        {
             return Status.Failure;
-        }
-        if (!loopAnimation)
-        {
-            companion.GetComponent<Animation>().wrapMode = WrapMode.ClampForever;
-        }
-        else if (loopAnimation)
-        {
-            companion.GetComponent<Animation>().wrapMode = WrapMode.Loop;
-        }
 
-        companion.GetComponent<Animation>().CrossFade(animationName);
+        if (!loopAnimation)
+            companion.GetComponent<Animation>().wrapMode = WrapMode.ClampForever;
+
+        else if (loopAnimation)
+            companion.GetComponent<Animation>().wrapMode = WrapMode.Loop;
+
+        if (!waitForAnimationEnd)
+            companion.GetComponent<Animation>().CrossFade(animationName);
+
+        else if (waitForAnimationEnd)
+            companion.GetComponent<Animation>().CrossFadeQueued(animationName);
+
         return Status.Success;
     }
+    
 }
---
+++ Assets/Scripts/LeafNodes/DestroyGameObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+using System;
+
+public class DestroyGameObject : ActionNode
+{
+    public GameObject objectToDestroy;
+    public override Status Update()
+    {
+        objectToDestroy.GetComponent<MeshRenderer>().enabled = false;
+        return Status.Success;
+    }
+}
