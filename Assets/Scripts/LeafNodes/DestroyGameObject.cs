ba4c021327be613daf695a79920fd7987acb2519
Maddie <mk186442@falmouth.ac.uk>
2017-04-20 13:23:28 +0100

    Prey is destroyed on collision with predator

---
+++ Assets/Scripts/LeafNodes/DestroyGameObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+using System;
+
+public class DestroyGameObject : ActionNode
+{
+    public GameObject objectToDestroy;
+    public override Status Update()
+    {
+        UnityEngine.Object.Destroy(objectToDestroy);
+        return Status.Success;
+    }
+}
