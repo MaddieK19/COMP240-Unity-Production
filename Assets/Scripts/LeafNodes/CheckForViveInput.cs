9494e220d2a59c7978697f07cc512b0d39c924c3
Maddie <mk186442@falmouth.ac.uk>
2017-04-22 11:19:05 +0100

    VR version takes Vive controller input instead of keyboard

--- Assets/Scripts/LeafNodes/CheckForViveInput.cs
+++ Assets/Scripts/LeafNodes/CheckForViveInput.cs
@@ -7,11 +7,13 @@ using System;
 public class CheckForViveInput : ActionNode
 {
     public SteamVR_TrackedController trackedController;
-
+    public String buttonToCheck;
 
     public override Status Update()
     {
-        if (trackedController.triggerPressed)
+        if (trackedController.triggerPressed && buttonToCheck == "trigger")
+            return Status.Success;
+        if (trackedController.gripped && buttonToCheck == "grip")
             return Status.Success;
         else
             return Status.Failure;
